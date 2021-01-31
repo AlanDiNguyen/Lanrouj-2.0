e4818b21c2eea9e9c07cff40035f962a4d61bebc
ics53 <Alan Nguyen>
2021-01-31 09:14:29 +0000

    Changing Meet to handle Cards

--- src/pages/Meet.js
+++ src/pages/Meet.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {IonContent, IonPage} from "@ionic/react";
-import useFormValidation from '../hooks/useFormValidation';
-import validateCreateProject from '../components/Product/validateCreateProduct';
 import firebase from '../firebase';
 import UserContext from '../contexts/UserContext';
 import SmallHeader from '../components/Header/SmallHeader';
 import LargeHeader from '../components/Header/LargeHeader';
 import {toast} from '../utils/toast';
 
+const INITIAL_STATE= {
+}
+
 const Meet = () => {
+  const {user} = React.useContext(UserContext);
+  const [submitting, setSubmitting] = React.useState(false);
   return (
     <IonPage>
       <SmallHeader title="Meet" />
