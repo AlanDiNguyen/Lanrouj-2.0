0378b22b7e421014c8326c3def67ac6ff8f3f168
ics53 <Alan Nguyen>
2021-01-31 09:47:32 +0000

    Added basic UserCards, need to further access user db to get info from it

---
+++ src/components/UserCards/UserCards.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {IonCard, IonCardContent} from '@ionic/react';
+
+
+const UserCard = ({ user }) => {
+  return (
+    <IonCard>
+      <img src=""/>
+      <ion-card-header>
+          <ion-card-title></ion-card-title>
+      </ion-card-header>
+      <ion-card-content>
+      </ion-card-content>
+    </IonCard>
+  );
+};
+
+
+export default UserCard;
