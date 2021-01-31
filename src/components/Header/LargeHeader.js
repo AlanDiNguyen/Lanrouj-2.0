51ed6dec7dadb5574bee8105786dea5faa40a631
ics53 <Alan Nguyen>
2021-01-31 00:54:11 +0000

    Added Header files and restructuring of Pages

---
+++ src/components/Header/LargeHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {IonHeader, IonTitle, IonToolbar} from '@ionic/react';
+
+const LargeHeader = ({title}) => {
+  return (
+    <IonHeader collapse="condense">
+    
+    </IonHeader>
+  )
+}
+
--- src/pages/Matches.js
+++ src/pages/Matches.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+import {IonContent, IonPage} from "@ionic/react";
+import SmallHeader from '../components/Header/SmallHeader';
+import LargeHeader from '../components/Header/LargeHeader';
 
 const Matches = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Matches</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <SmallHeader title="Matches" />
+      <IonContent fullscreen>
+        <LargeHeader title="Matches" />
+      </IonContent>
     </IonPage>
   )
 }
