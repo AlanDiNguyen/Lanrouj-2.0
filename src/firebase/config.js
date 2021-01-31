38a507c8748def51a9785e215b7380b09b2e1be9
Tugario <chiang.tiger1@gmail.com>
2021-01-30 16:27:36 -0800

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBbeil3rtM1m5JAZSZivut4nUpJlX72Y98",
+    authDomain: "lanruoj-fee8e.firebaseapp.com",
+    projectId: "lanruoj-fee8e",
+    storageBucket: "lanruoj-fee8e.appspot.com",
+    messagingSenderId: "219562293480",
+    appId: "1:219562293480:web:cb53b6ff1873e21d50315d",
+    measurementId: "G-6JDZKDLH6T"
+  };
+
+  export default firebaseConfig;
